0e668ae8adf1199b85882400ac9483a0ea9940da
Miot2J <ck96117@gmail.com>
2021-04-28 22:16:28 +0900

    ref: Titanic.py , feat IncreasingSequence.py
    Titanic.py deque 사용 하여 리펙토링

--- Inflearn/HalfSearch_Greedy/Titanic.py
+++ Inflearn/HalfSearch_Greedy/Titanic.py
@@ -1,16 +1,20 @@
-n , limit = map(int,input().split())
-p = list(map(int,input.split()))
+from collections import deque
+
+n, limit = map(int, input().split())
+p = list(map(int, input().split()))
 p.sort()
+# list -> deque 자료형으로 변환 deque는 자료 앞뒤로 넣고 빼기 가능(포인터 이동방식)
+p = deque(p)
 cnt = 0
 while p:
-    if len(p) ==1:
-        cnt +=1
+    if len(p) == 1:
+        cnt += 1
         break
-    if p[0]+p[-1] >limit:
+    if p[0] + p[-1] > limit:
         p.pop()
-        cnt +=1
+        cnt += 1
     else:
-        p.pop(0)
+        p.popleft()
         p.pop()
-        cnt +=1
+        cnt += 1
 print(cnt)
