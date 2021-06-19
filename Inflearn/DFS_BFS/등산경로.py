27615cb34b4639500720838f5eb2009f274622de
Miot2J <ck96117@gmail.com>
2021-06-19 23:57:03 +0900

    feat: 미로탐색.py

--- Inflearn/DFS_BFS/등산경로.py
+++ Inflearn/DFS_BFS/등산경로.py
@@ -20,20 +20,20 @@ if __name__ == "__main__":
     n = int(input())
     board = [[0] * n for _ in range(n)]
     ch = [[0] * n for _ in range(n)]
-max = -2147000000
-min = 2147000000
-for i in range(n):
-    tmp = list(map((int, input().split())))
-    for j in range(n):
-        if tmp[j] < min:
-            min = tmp[j]
-            sx = i
-            sy = j
-        if tmp[j] > max:
-            max = tmp[j]
-            ex = i
-            ey = j
-        board[i][j] = tmp[j]
-
-        cnt =0
+    max = -2147000000
+    min = 2147000000
+    for i in range(n):
+        tmp = list(map((int, input().split())))
+        for j in range(n):
+            if tmp[j] < min:
+                min = tmp[j]
+                sx = i
+                sy = j
+            if tmp[j] > max:
+                max = tmp[j]
+                ex = i
+                ey = j
+            board[i][j] = tmp[j]
+    ch[sx][sy] =1
+    cnt =0
 print(cnt)
